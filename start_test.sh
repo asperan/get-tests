0359cdb9b34cf6316b422d4b7317aba6f73c63d3
Alex Speranza <alex.speranza@studio.unibo.it>
2023-06-23 18:58:26 +0200

    fix: fix typo

--- start_test.sh
+++ start_test.sh
@@ -2,7 +2,7 @@
 
 git branch | grep "${TEST_BRANCH}" &>/dev/null
 
-if ["$?" -eq "0" ] ; then
+if [ "$?" -eq "0" ] ; then
   git checkout "${TEST_BRANCH}" &> /dev/null
 else
   echo "Branch '${TEST_BRANCH}' does not exists. Checking out 'main'" >&2
