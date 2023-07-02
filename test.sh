cb1b9a91115c99cf4e0ffa8d074700b8f4c0689e
Alex Speranza <alex.speranza@studio.unibo.it>
2023-07-02 18:26:43 +0200

    test: fix comparison between versions

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ DEFAULT_FIRST_VERSION="0.1.0"
 
 DESCRIBED_VERSION="$(get describe --exclude-metadata)"
 
-test "${DESCRIBED_VERSION}" -eq "${DEFAULT_FIRST_VERSION}"
+test "${DESCRIBED_VERSION}" = "${DEFAULT_FIRST_VERSION}"
 
 exit $?
