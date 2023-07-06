dd721c073bf5813dae3fafd9b551ddc0e76e1416
Alex Speranza <alex.speranza@studio.unibo.it>
2023-07-06 16:38:56 +0200

    test: add test for a breaking commit

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-echo "Main branch, no test defined here" >&2
-exit 1
+EXPTECTED_COMMIT_MESSAGE="feat!: add a new text file"
+
+touch new_file.txt
+
+git add new_file.txt
+
+get commit -t 'feat' -S '' -s 'add a new text file' -m '' --breaking -q
+
+LAST_COMMIT_MESSAGE="$(git log --format=%s | head -n 1)"
+
+[ "$LAST_COMMIT_MESSAGE" = "${EXPTECTED_COMMIT_MESSAGE}" ]
+
+exit $?
+
