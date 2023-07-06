06ac842432d80ffd29e585b974a9dd28c162864b
Alex Speranza <alex.speranza@studio.unibo.it>
2023-07-06 22:13:10 +0200

    test: add test for a custom prerelease pattern

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Main branch, no test defined here" >&2
-exit 1
+EXPECTED_VERSION="0.1.1-alpha1"
+
+CALCULATED_VERSION="$(get describe --prerelease --exclude-metadata --prerelease-pattern "alpha(p)")"
+
+[ "${EXPECTED_VERSION}" = "${CALCULATED_VERSION}" ]
+
+exit $?
+
