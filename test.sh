186b90b4e5ac3d3890ed432779f1c1980c14f16d
Alex Speranza <alex.speranza@studio.unibo.it>
2023-07-06 23:04:34 +0200

    test: add test for checking the reset of a prerelease when a greater change is committed

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Main branch, no test defined here" >&2
-exit 1
+EXPECTED_VERSION="0.2.0-dev1"
+
+CALCULATED_VERSION="$(get describe --exclude-metadata --prerelease)"
+
+[ "${EXPECTED_VERSION}" = "${CALCULATED_VERSION}" ]
+
+exit $?
+
